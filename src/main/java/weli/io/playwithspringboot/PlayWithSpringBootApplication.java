7ef3eaaa92173854a0823f4b4f6c494cbe9b3e64
Weinan Li <l.weinan@gmail.com>
2017-12-21 00:35:35 +0800

    .

---
+++ src/main/java/weli/io/playwithspringboot/PlayWithSpringBootApplication.java
@@ -0,0 +1,12 @@
+package weli.io.playwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWithSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/weli/io/playwithspringboot/PlayWithSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package weli.io.playwithspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayWithSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
